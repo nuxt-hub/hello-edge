36b719cbf47b2eec5e2fa77d558a7726bc69e9b3
Sébastien Chopin <seb@nuxt.com>
2024-10-21 10:51:24 +0200

    chore: udpate

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,35 +1,34 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-07-30',
-  // https://nuxt.com/docs/getting-started/upgrade#testing-nuxt-4
-  future: { compatibilityVersion: 4 },
-
   // https://nuxt.com/modules
   modules: [
     '@nuxthub/core',
-    '@nuxt/eslint'
+    '@nuxt/eslint',
   ],
 
-  // https://hub.nuxt.com/docs/getting-started/installation#options
-  hub: {},
+  // https://devtools.nuxt.com
+  devtools: { enabled: true },
 
   // Env variables - https://nuxt.com/docs/getting-started/configuration#environment-variables-and-private-tokens
   runtimeConfig: {
     public: {
       // Can be overridden by NUXT_PUBLIC_HELLO_TEXT environment variable
-      helloText: 'Hello from the Edge 👋'
-    }
+      helloText: 'Hello from the Edge 👋',
+    },
   },
+  // https://nuxt.com/docs/getting-started/upgrade#testing-nuxt-4
+  future: { compatibilityVersion: 4 },
+  compatibilityDate: '2024-07-30',
+
+  // https://hub.nuxt.com/docs/getting-started/installation#options
+  hub: {},
 
   // https://eslint.nuxt.com
   eslint: {
     config: {
       stylistic: {
-        quotes: 'single'
-      }
-    }
+        quotes: 'single',
+      },
+    },
   },
-
-  // https://devtools.nuxt.com
-  devtools: { enabled: true }
 })
